295b9fadcc910162455a31b758e24a6d368a6420
Harathi <harathilohit12@gmail.com>
2020-07-28 20:20:08 +0000

    updated

---
+++ src/main/java/com/tictac/co/App.java
@@ -0,0 +1,13 @@
+package com.tictac.co;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
